2e2e16be8da5ae2df015664154e7941b79eb2284
Sourabh singh <96938361+SouSingh@users.noreply.github.com>
2024-09-09 11:17:15 +0530

    Update check.py

--- check.py
+++ check.py
@@ -153,4 +153,4 @@ async def root():
     return {"message": "API is up and running"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
